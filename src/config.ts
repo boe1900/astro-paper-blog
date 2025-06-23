313763f35f6cdb3cb42a5d5e4e40ee8c9ef9a54e
huab <huab@rockycore.cn>
2025-06-23 10:57:01 +0800

    first commit

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  website: "https://blog.cola1900.eu.org/", // replace this with your deployed domain
+  author: "Dola",
+  profile: "",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Dola's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
